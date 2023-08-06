8600c41c3ebd4e0513283f1c0a4c6c61e672c787
BerkovskiiAlex <berkovskii845@gmail.com>
2023-08-06 22:21:00 +0300

    запрос сделан, начинаю настраивать Seachbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,50 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
-//
+import React from 'react';
+
+import { Button } from './Button/Button';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+import { Loader } from './Loader/Loader';
+import { Modal } from './Modal/Modal';
+import { Searchbar } from './Searchbar/Searchbar';
+import { fetchImg } from '../service/api';
+
+export class App extends React.Component {
+  state = {
+    q: 'cat',
+    page: 1,
+    images: [],
+  };
+
+  async componentDidMount() {
+    const { q, page } = this.state;
+    try {
+      const images = await fetchImg({
+        q,
+        page,
+      });
+
+      this.setState({ images });
+      console.log(this.state);
+    } catch (error) {}
+  }
+
+  render() {
+    return (
+      <section>
+        <Searchbar />
+        <ImageGallery />
+        <ImageGalleryItem />
+        <Loader />
+        <Button />
+        <Modal />
+      </section>
+    );
+  }
+}
+
+// .App {
+//   display: grid;
+//   grid-template-columns: 1fr;
+//   grid-gap: 16px;
+//   padding-bottom: 24px;
+// }
