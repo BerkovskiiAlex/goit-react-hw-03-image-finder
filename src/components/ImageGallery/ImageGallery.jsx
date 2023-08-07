cfab55bbac806a08e41012adf35c4e700058e09b
BerkovskiiAlex <berkovskii845@gmail.com>
2023-08-07 19:39:51 +0300

    ImageGallery.styled

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
+import { ImageGalleryStyled } from './ImageGalleryStyled';
 
 export const ImageGallery = ({ children }) => {
-  return <ul className="gallery">{children}</ul>;
+  return (
+    <ImageGalleryStyled className="gallery">{children}</ImageGalleryStyled>
+  );
 };
-
-// .ImageGallery {
-//   display: grid;
-//   max-width: calc(100vw - 48px);
-//   grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
-//   grid-gap: 16px;
-//   margin-top: 0;
-//   margin-bottom: 0;
-//   padding: 0;
-//   list-style: none;
-//   margin-left: auto;
-//   margin-right: auto;
-// }
